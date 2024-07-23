75ef23d4b2e80639a086a3647caee572b6e2eaf7
Mangoubi <dmangoubi@hbs.edu>
2024-07-23 15:48:01 -0400

    Push R folder

---
+++ R/Functions/flatten_json.R
@@ -0,0 +1,25 @@
+# Flatten JSON from GLEIF API
+# Purpose: Flatten JSON to be a row of data from the GLEIF API 
+# Input: 
+#     JSON data from GLEIF API
+# Output: 
+#     Flattened data. One row of data than can be appended together to create   
+#     a dataframe of LEIs
+#
+#
+# Contact: BRDS (Caracuzzo, Alex acaracuzzo@hbs.edu)
+# Repo: https://github.com/hbs-brds
+# Website: https://www.library.hbs.edu/baker-research-data-services 
+
+flatten_json <- function(data, parent_key = '', sep = ' ') {
+  items <- list()
+  for (k in names(data)) {
+    new_key <- ifelse(parent_key == '', k, paste(parent_key, k, sep = sep))
+    if (is.list(data[[k]])) {
+      items <- c(items, flatten_json(data[[k]], new_key, sep))
+    } else {
+      items[[new_key]] <- data[[k]]
+    }
+  }
+  return(items)
+}
---
+++ R/Functions/gleifapi_getdata_fromlei.R
@@ -0,0 +1,32 @@
+# leipull_gleifapi
+# Purpose: Pull Data for one LEI from the GLEIF API, return a clean row of data on that LEI
+# Input: 
+#     JSON data from GLEIF API
+# Output: 
+#     Flattened data. One row of data than can be appended together to create   
+#     a dataframe of LEIs
+#
+#
+# Contact: BRDS (Caracuzzo, Alex acaracuzzo@hbs.edu)
+# Repo: https://github.com/hbs-brds
+# Website: https://www.library.hbs.edu/baker-research-data-services 
+
+
+# Function
+gleifapi_getdata_fromlei  <- function(lei) {
+  # Set API information for this specific LEI
+  url <- paste0("https://api.gleif.org/api/v1/lei-records/",lei)
+  res <- curl_fetch_memory(url)
+  
+  #Pause for API Limits
+  Sys.sleep(1.3)
+  
+  # Get data, format
+  content <- rawToChar(res$content)
+  parsed_data <- fromJSON(content)
+  flattened_data <- flatten_json(parsed_data)
+  transpose_flat <- t(flattened_data)
+  transpose_flat <- as.data.frame(transpose_flat)
+  
+  return(transpose_flat)
+}
